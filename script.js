8a8d46d9e19c2e13f77552cae6ae7087d2140aa7
Divyanshu-Dhudhuani <divyanshudhudhuani@gmail.com>
2026-09-05 17:24:04 +0530

    Fixed title bar for each webpage

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 function scrollTitle(titleText, speed = 500) {
   // Add spacing so the end and start of the text don't stick together
   let text = titleText + "   •   "; 
-
+if (window.location.pathname.endsWith('index.html') || window.location.pathname.endsWith('/')) {
   setInterval(() => {
     // Move the first character to the end of the string
+    
     text = text.substring(1) + text.substring(0, 1);
     document.title = text;
   }, speed);
+}
 }
 
 // Pass your club or hackathon website title here
